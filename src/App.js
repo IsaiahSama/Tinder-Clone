65a8bc19c48f81ba3fb90852018ac9f1617af076
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-13 17:12:48 -0400

    Added Header styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
-
       {/* Header */}
       <Header />
       {/* Tinder Cards */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Header.css";
+import PersonIcon from "@mui/icons-material/Person";
+import ForumIcon from "@mui/icons-material/Forum";
+import { IconButton } from "@mui/material";
 
 function Header() {
   return (
     <div className="header">
-      <h2>Header</h2>
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
+        alt="Tinder Logo"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 }
