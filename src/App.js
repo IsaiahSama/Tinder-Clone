93ecbb342f880e4ff16a625a03bb7cbd4cbe9910
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-13 16:48:22 -0400

    Setup app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Hello World</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      {/* Buttons Below cards  */}
+
+      {/* Chats Screen */}
+      {/* Individual Chat Screen  */}
     </div>
   );
 }
