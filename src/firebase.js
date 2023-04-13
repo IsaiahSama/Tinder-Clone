69cab3bc0e66a973c383b38a4ef59219fba12de3
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-13 13:42:15 -0400

    Added core files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCh3HahmEHo7WwOyrt03PJ6rTyMo9srBvA",
+  authDomain: "tinder-clone-a9ef7.firebaseapp.com",
+  projectId: "tinder-clone-a9ef7",
+  storageBucket: "tinder-clone-a9ef7.appspot.com",
+  messagingSenderId: "911298608528",
+  appId: "1:911298608528:web:06f319d1e7c2381f0dc715",
+  measurementId: "G-N1REKM2803",
+};
